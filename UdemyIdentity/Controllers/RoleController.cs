cedb4dca462b49a872e500a4dba38e3cacc032f5
savasevtr <savas.ev@yahoo.com>
2020-10-29 23:15:21 +0300

    kullanıcı rollerinin listelenmesi

--- UdemyIdentity/Controllers/RoleController.cs
+++ UdemyIdentity/Controllers/RoleController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -14,10 +15,12 @@ namespace UdemyIdentity.Controllers
     public class RoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public RoleController(RoleManager<AppRole> roleManager)
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -107,6 +110,33 @@ namespace UdemyIdentity.Controllers
             TempData["Errors"] = identityResult.Errors;
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList()
+        {
+            return View(_userManager.Users.ToList());
+        }
+
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(u => u.Id == id);
+
+            var roles = _roleManager.Roles.ToList();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            List<RoleAssignViewModel> models = new List<RoleAssignViewModel>();
+
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = item.Id;
+                model.Name = item.Name;
+                model.Exists = userRoles.Contains(item.Name);
+                models.Add(model);
+            }
+
+            return View(models);
         }
     }
 }
