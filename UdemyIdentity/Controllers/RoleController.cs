22fb817d01095aebcb7344d3dbd93a7f84c78de4
savasevtr <savas.ev@yahoo.com>
2020-10-29 21:31:10 +0300

    Rol Listeleme

---
+++ UdemyIdentity/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemyIdentity.Context;
+
+namespace UdemyIdentity.Controllers
+{
+    [Authorize]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+    }
+}
