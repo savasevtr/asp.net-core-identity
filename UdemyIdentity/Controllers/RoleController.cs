d41b8abc0dc1f9e2da0c22c30e92ae4394b6fd5c
savasevtr <savas.ev@yahoo.com>
2020-10-29 21:54:14 +0300

    Rol Ekleme

--- UdemyIdentity/Controllers/RoleController.cs
+++ UdemyIdentity/Controllers/RoleController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UdemyIdentity.Context;
+using UdemyIdentity.Models;
 
 namespace UdemyIdentity.Controllers
 {
@@ -22,6 +23,37 @@ namespace UdemyIdentity.Controllers
         public IActionResult Index()
         {
             return View(_roleManager.Roles.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View(new RoleViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = model.Name
+                };
+
+                var identityResult = await _roleManager.CreateAsync(role);
+
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var item in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
---
+++ UdemyIdentity/Models/RoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentity.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = "Rol adı gereklidir!")]
+        [Display(Name = "Rol Adı:")]
+        public string Name { get; set; }
+    }
+}
